1d9e9c941c28e25fd46d9ca7be7c40a1b09d809e
zhangyingwei <723129bao>
2016-10-25 17:28:03 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="wrapper">
+    <main-header></main-header>
+    <main-sidebar></main-sidebar>
+    <content-wrapper></content-wrapper>
+    <main-footer></main-footer>
+    <control-sidebar></control-sidebar>
+  </div>
+</template>
+
+<script>
+import MainHeader from './components/header/MainHeader'
+import MainSidebar from './components/sidebar/MainSidebar'
+import ContentWrapper from './components/content/ContentWrapper'
+import MainFooter from './components/footer/MainFooter'
+import ControlSidebar from './components/control/ControlSidebar'
+
+export default {
+  components: {
+    MainHeader,
+    MainSidebar,
+    ContentWrapper,
+    MainFooter,
+    ControlSidebar
+  }
+}
+</script>
+
+<style>
+@import './assets/bootstrap/css/bootstrap.min.css';
+@import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css';
+@import 'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css';
+@import './assets/AdminLTE/css/AdminLTE.css';
+@import './assets/AdminLTE/css/skins/skin-blue.min.css';
+</style>
